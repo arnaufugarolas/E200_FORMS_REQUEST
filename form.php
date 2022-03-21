60a4bfa23d919c9fd5ab99b5f3cf30356fe22476
ozamoramerino <oriol.zamora@mataro.epiaedu.cat>
2022-03-21 20:15:27 +0100

    validar

--- form.php
+++ form.php
@@ -1,3 +1,5 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
 <?php
 if ((!isset($_GET["nom"]) or ($_GET["nom"] === '') or (preg_match('/[^a-zA-Z]/', $_GET["nom"])))) {
     header('Location: index.html');
--- validar.php
+++ validar.php
@@ -1,3 +1,5 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
 <?php $correcte = (isset($_GET["nom"]) and isset($_GET["cognoms"]) and isset($_GET["nif"]) and isset($_GET["poblacio"]) and isset($_GET["codi_postal"]) and isset($_GET["data_naixament"]) and isset($_GET["email"]) and isset($_GET["telefon"])) ;?>
 <!DOCTYPE html>
 <html lang="es">
@@ -14,57 +16,72 @@
 } ?>
 
 <form id="index" name="form" method="GET" action="form.php">
-    <div>
-        <label for="form_nom" <?php if (!isset($_GET["nom"])) {echo 'class="error"';}?>>Nom</label>
-        <input type="text" name="nom" id="form_nom" required readonly value="<?php echo $_GET["nom"] ?? '';?>">
+    <div class="card">
+        <div class="card-body" >
+            <?php echo $_GET["nom"] ?? '';?>
+        </div>
     </div>
-    <div>
-        <label for="form_cognoms" <?php if (!isset($_GET["cognoms"])) {echo 'class="error"';}?>>Cognoms</label>
-        <input type="text" name="cognoms" id="form_cognoms" required readonly value="<?php echo $_GET["cognoms"] ?? '';?>">
+
+    <div class="card">
+        <div class="card-body" >
+            <?php echo $_GET["cognoms"] ?? '';?>
+        </div>
     </div>
-    <div>
-        <label for="form_nif" <?php if (!isset($_GET["nif"])) {echo 'class="error"';}?>>NIF</label>
-        <input type="text" name="nif" id="form_nif" required readonly value="<?php echo $_GET["nif"] ?? '';?>">
+    
+    <div class="card">
+        <div class="card-body" >
+            <?php echo $_GET["nif"] ?? '';?>
+        </div>
     </div>
-    <div>
-        <label for="form_poblacio" <?php if (!isset($_GET["poblacio"])) {echo 'class="error"';}?>>Població</label>
-        <input type="text" name="poblacio" id="form_poblacio" required readonly value="<?php echo $_GET["poblacio"] ?? '';?>">
+
+    <div class="card">
+        <div class="card-body" >
+            <?php echo $_GET["poblacio"] ?? '';?>
+        </div>
     </div>
-    <div>
-        <label for="form_codi" <?php if (!isset($_GET["codi_postal"])) {echo 'class="error"';}?>>Codi Postal</label>
-        <input type="text" name="codi_postal" id="form_codi" required readonly value="<?php echo $_GET["codi_postal"] ?? '';?>">
+    
+    <div class="card">
+        <div class="card-body" >
+            <?php echo $_GET["codi_postal"] ?? '';?>
+        </div>
     </div>
-    <div>
-        <label for="form_data" <?php if (!isset($_GET["data_naixament"])) {echo 'class="error"';}?>>Data Naixement</label>
-        <input type="date" name="data_naixament" id="form_data" required readonly value="<?php echo $_GET["data_naixament"] ?? '';?>">
+
+    <div class="card">
+        <div class="card-body" >
+            <?php echo $_GET["codi_postal"] ?? '';?>
+        </div>
     </div>
-    <div>
-        <label for="form_email" <?php if (!isset($_GET["email"])) {echo 'class="error"';}?>>Email</label>
-        <input type="email" name="email" id="form_email" required readonly value="<?php echo $_GET["email"] ?? '';?>">
+
+    <div class="card">
+        <div class="card-body" >
+            <?php echo $_GET["data_naixament"] ?? '';?>
+        </div>
     </div>
-    <div>
-        <label for="form_telefon" <?php if (!isset($_GET["telefon"])) {echo 'class="error"';}?>>Telèfon</label>
-        <input type="text" name="telefon" id="form_telefon" required readonly value="<?php echo $_GET["telefon"] ?? '';?>">
+    
+    <div class="card">
+        <div class="card-body" >
+            <?php echo $_GET["email"] ?? '';?>
+        </div>
     </div>
-    <div>
-        <label for="form_sexe" <?php if (!isset($_GET["sexe"])) {echo 'class="error"';}?>>Sexe</label>
-        <datalist id="sexe">
-            <option>Home</option>
-            <option>Dona</option>
-        </datalist>
-        <input type="text" name="sexe" id="form_sexe" list="sexe" required readonly value="<?php echo $_GET["sexe"] ?? '';?>">
+
+    <div class="card">
+        <div class="card-body" >
+            <?php echo $_GET["telefon"] ?? '';?>
+        </div>
     </div>
-    <div>
-        <label for="form_civil" <?php if (!isset($_GET["civil"])) {echo 'class="error"';}?>>Estat Civil</label>
-        <datalist id="civil">
-            <option>Casat</option>
-            <option>Solter</option>
-            <option>Vidu</option>
-            <option>Separat</option>
-            <option>Altres</option>
-        </datalist>
-        <input type="text" name="civil" id="form_civil" list="civil" required readonly value="<?php echo $_GET["civil"] ?? '';?>">
+
+    <div class="card">
+        <div class="card-body" >
+            <?php echo $_GET["sexe"] ?? '';?>
+        </div>
     </div>
+    
+    <div class="card">
+        <div class="card-body" >
+            <?php echo $_GET["civil"] ?? '';?>
+        </div>
+    </div>
+    
     <button type="submit">EDITAR</button>
 
     <?php if ($correcte) {
