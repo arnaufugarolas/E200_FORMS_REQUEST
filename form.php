04d9de7d823d24f58e3e227807abeb6a93da9f15
arnaufugarolas <arnau.fugarolas@mataro.epiaedu.cat>
2022-04-01 13:33:16 +0200

    Finish

--- form.php
+++ form.php
@@ -1,23 +1,22 @@
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
 <?php
 if ((!isset($_GET["nom"]) or ($_GET["nom"] === '') or (preg_match('/[^a-zA-Z]/', $_GET["nom"])))) {
     header('Location: index.html');
     exit;
 }
 ?>
+
 <!DOCTYPE html>
 <html lang="es">
 <head>
     <meta charset="UTF-8">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <title>Index</title>
+    <title>Form</title>
 </head>
-<body>
+<body style="overflow: hidden; margin-top: -2%">
 <form class="p-5" id="index" name="form" method="GET" action="validar.php">
     <div class="col-md-6 input-group p-2">
-        <label class="form-label input-group p-2" for="form_nom">Nom</label>
-        <input class="form-control input-group p-2" type="text" name="nom" id="form_nom" required readonly value="<?php echo $_GET["nom"] ?? '';?>">
+        <label class="form-label input-group" for="form_nom">Nom</label>
+        <input class="form-control input-group" type="text" name="nom" id="form_nom" required readonly value="<?php echo $_GET["nom"] ?? '';?>">
     </div>
     <div class="col-md-6 input-group p-2">
         <label class="form-label input-group" for="form_cognoms">Cognoms</label>
@@ -29,15 +28,15 @@ if ((!isset($_GET["nom"]) or ($_GET["nom"] === '') or (preg_match('/[^a-zA-Z]/',
     </div>
     <div class="col-md-6 input-group p-2">
         <label class="form-label input-group" for="form_poblacio">Població</label>
-        <input class="form-control" type="text" name="poblacio" id="form_poblacio" required value="<?php echo $_GET["poblacio"] ?? '';?>">
+        <input class="form-control" type="text" name="poblacio" id="form_poblacio" value="<?php echo $_GET["poblacio"] ?? '';?>">
     </div>
     <div class="col-md-6 input-group p-2">
         <label class="form-label input-group" for="form_codi">Codi Postal</label>
-        <input class="form-control" type="text" name="codi_postal" id="form_codi" required value="<?php echo $_GET["codi_postal"] ?? '';?>">
+        <input class="form-control" type="text" name="codi_postal" id="form_codi" value="<?php echo $_GET["codi_postal"] ?? '';?>">
     </div>
-    <div class="col-md-6 input-group p-2"> 
-        <label class="datepicker input-group" data-date-format="mm/dd/yyyy" for="form_data">Data Naixement</label>
-        <input class="form-control" type="date" name="data_naixament" id="form_data" required value="<?php echo $_GET["data_naixament"] ?? '';?>">
+    <div class="col-md-6 input-group p-2">
+        <label class="datepicker input-group" for="form_data">Data Naixement</label>
+        <input class="form-control" type="date" name="data_naixement" id="form_data" value="<?php echo $_GET["data_naixement"] ?? '';?>">
     </div>
     <div class="col-md-6 input-group p-2">
         <label class="form-label input-group" for="form_email">Email</label>
@@ -48,21 +47,40 @@ if ((!isset($_GET["nom"]) or ($_GET["nom"] === '') or (preg_match('/[^a-zA-Z]/',
         <input class="form-control" type="text" name="telefon" id="form_telefon" required value="<?php echo $_GET["telefon"] ?? '';?>">
     </div>
     <div class="col-md-6 input-group p-2">
-        <label for="sexe" class="form-label input-group">Sexe</label>
-        <select class="form-select" aria-label="Default select example"  id="sexe">
-            <option>Home</option>
-            <option>Dona</option>
+        <label for="form_sexe" class="form-label input-group">Sexe</label>
+        <select class="form-select" id="form_sexe" name="sexe">
+            <?php
+            if ($_GET["sexe"] ?? '' === 'Dona') {
+                echo '<option>Home</option> <option selected hidden>Dona</option>';
+            }
+            else {
+                echo '<option selected>Home</option> <option>Dona</option>';
+            }
+            ?>
         </select>
     </div>
     <div class="col-auto input-group p-2">
         <label class="input-group" for="form_civil">Estat Civil</label>
-        <select class="form-select" aria-label="Default select example" id="civil">
-            <option>Casat</option>
-            <option>Solter</option>
-            <option>Vidu</option>
-            <option>Separat</option>
-            <option>Altres</option>
-        </select>      
+        <select class="form-select" id="form_civil" name="civil">
+            <?php
+            $civil = $_GET["civil"] ?? '';
+            if ($civil === 'Solter') {
+                echo '<option>Casat</option><option selected>Solter</option><option>Vidu</option><option>Separat</option><option>Altres</option>';
+            }
+            elseif ($civil === 'Vidu') {
+                echo '<option>Casat</option><option>Solter</option><option selected>Vidu</option><option>Separat</option><option>Altres</option>';
+            }
+            elseif ($civil === 'Separat') {
+                echo '<option>Casat</option><option>Solter</option><option>Vidu</option><option selected>Separat</option><option>Altres</option>';
+            }
+            elseif ($civil === 'Altres') {
+                echo '<option>Casat</option><option>Solter</option><option>Vidu</option><option>Separat</option><option selected>Altres</option>';
+            }
+            else {
+                echo '<option selected>Casat</option><option>Solter</option><option>Vidu</option><option>Separat</option><option>Altres</option>';
+            }
+            ?>
+        </select>
     </div>
     <button class="btn btn-primary" type="submit">RESUM</button>
 </form>
--- validar.php
+++ validar.php
@@ -1,91 +1,171 @@
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
-<?php $correcte = (isset($_GET["nom"]) and isset($_GET["cognoms"]) and isset($_GET["nif"]) and isset($_GET["poblacio"]) and isset($_GET["codi_postal"]) and isset($_GET["data_naixament"]) and isset($_GET["email"]) and isset($_GET["telefon"])) ;?>
 <!DOCTYPE html>
 <html lang="es">
 <head>
     <meta charset="UTF-8">
     <title>Index</title>
-    <link href="validar.css" rel="stylesheet">
-
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
-<body>
-<?php if (!$correcte) {
-    echo '<h2 class="error">Formulari incorrecte</h2>';
-} ?>
-
+<body style="margin: 2%">
 <form id="index" name="form" method="GET" action="form.php">
+    <input type="text" name="nom" id="form_nom" hidden readonly value="<?php echo $_GET["nom"] ?? '';?>">
+    <input type="text" name="cognoms" id="form_cognoms" hidden readonly value="<?php echo $_GET["cognoms"] ?? '';?>">
+    <input type="text" name="nif" id="form_nif" hidden readonly value="<?php echo $_GET["nif"] ?? '';?>">
+    <input type="text" name="poblacio" id="form_poblacio" hidden readonly value="<?php echo $_GET["poblacio"] ?? '';?>">
+    <input type="text" name="codi_postal" id="form_codi" hidden readonly value="<?php echo $_GET["codi_postal"] ?? '';?>">
+    <input type="date" name="data_naixement" id="form_data" hidden readonly value="<?php echo $_GET["data_naixement"] ?? '';?>">
+    <input type="email" name="email" id="form_email" hidden readonly value="<?php echo $_GET["email"] ?? '';?>">
+    <input type="text" name="telefon" id="form_telefon" hidden readonly value="<?php echo $_GET["telefon"] ?? '';?>">
+    <input type="text" name="sexe" id="form_sexe" hidden readonly value="<?php echo $_GET["sexe"] ?? '';?>">
+    <input type="text" name="civil" id="form_civil" hidden readonly value="<?php echo $_GET["civil"] ?? '';?>">
+
+    <?php $correcte = true ;?>
+
     <div class="card">
-        <div class="card-body" >
-            <?php echo $_GET["nom"] ?? '';?>
-        </div>
+        <label class="form-label input-group" for="form_nom">Nom</label>
+        <?php
+        $check = $_GET["nom"] ?? '';
+
+        if ((($check === '') || (preg_match('/[^a-zA-Z]/', $check)))) {
+            echo '<div class="card-body alert alert-danger">';
+            $correcte = false;
+        }
+        else { echo '<div class="card-body alert alert-success">'; }
+
+        if ($check === '') { echo 'Falta el nom'; }
+        else { echo $check; }
+
+        echo '</div>'
+        ?>
     </div>
 
     <div class="card">
-        <div class="card-body" >
-            <?php echo $_GET["cognoms"] ?? '';?>
-        </div>
+        <label class="form-label input-group" for="form_cognoms">Cognoms</label>
+        <?php
+        $check = $_GET["cognoms"] ?? '';
+
+        if ((($check === '') || (!preg_match('/^[a-zA-Z]+(?:\s[a-zA-Z]+)?$/', $check)))) {
+            echo '<div class="card-body alert alert-danger">';
+            $correcte = false;
+        }
+        else { echo '<div class="card-body alert alert-success" >'; }
+
+        if ($check === '') { echo 'Falten els cognoms'; }
+        else { echo $check; }
+
+        echo '</div>'
+        ?>
     </div>
 
     <div class="card">
-        <div class="card-body" >
-            <?php echo $_GET["nif"] ?? '';?>
-        </div>
+        <label class="form-label input-group" for="form_nif">NIF</label>
+        <?php
+        $check = $_GET["nif"] ?? '';
+
+        if ((($check === '') || (!preg_match('/^[\d]{8}[a-zA-Z]$/', $check)))) {
+            echo '<div class="card-body alert alert-danger">';
+            $correcte = false;
+        }
+        else { echo '<div class="card-body alert alert-success">'; }
+
+        if ($check === '') { echo 'Falta el NIF'; }
+        else { echo $check; }
+        echo '</div>'
+        ?>
     </div>
 
     <div class="card">
-        <div class="card-body" >
-            <?php echo $_GET["poblacio"] ?? '';?>
-        </div>
+        <label class="form-label input-group" for="form_poblacio">Població</label>
+        <?php
+        $check = $_GET["poblacio"] ?? '';
+
+        if ($check === '') { echo '<div class="card-body alert alert-warning">Falta la població</div>'; }
+        else { echo '<div class="card-body alert alert-info">'.$check.'</div>'; }
+        ?>
     </div>
 
     <div class="card">
-        <div class="card-body" >
-            <?php echo $_GET["codi_postal"] ?? '';?>
-        </div>
+        <label class="form-label input-group" for="form_codi">Codi Postal</label>
+        <?php
+        $check = $_GET["codi_postal"] ?? '';
+
+        if ($check === '') { echo '<div class="card-body alert alert-warning">Falta el codi postal</div>'; }
+        else { echo '<div class="card-body alert alert-info">'.$check.'</div>'; }
+        ?>
     </div>
 
     <div class="card">
-        <div class="card-body" >
-            <?php echo $_GET["codi_postal"] ?? '';?>
-        </div>
+        <label class="form-label input-group" for="form_data">Data Naixement</label>
+        <?php
+        $check = $_GET["data_naixement"] ?? '';
+
+        if ($check === '') { echo '<div class="card-body alert alert-warning">Falta la data de naixement</div>'; }
+        else { echo '<div class="card-body alert alert-info">'.$check.'</div>'; }
+        ?>
     </div>
 
     <div class="card">
-        <div class="card-body" >
-            <?php echo $_GET["data_naixament"] ?? '';?>
-        </div>
+        <label class="form-label input-group" for="form_email">Email</label>
+        <?php
+        $check = $_GET["email"] ?? '';
+
+        if ((($check === '') || (!preg_match('/^[a-zA-Z\d+_.-]+@[a-zA-Z\d.-]+$/', $check)))) {
+            echo '<div class="card-body alert alert-danger">';
+            $correcte = false;
+        }
+        else { echo '<div class="card-body alert alert-success">'; }
+
+        if ($check === '') { echo 'Falta l\'email'; }
+        else { echo $check; }
+        echo '</div>'
+        ?>
     </div>
 
     <div class="card">
-        <div class="card-body" >
-            <?php echo $_GET["email"] ?? '';?>
-        </div>
+        <label class="form-label input-group" for="form_telefon">Telèfon</label>
+        <?php
+        $check = $_GET["telefon"] ?? '';
+
+        if ((($check === '') || (!preg_match('/^[+]?[(]?[\d]{3}[)]?[-\s.]?[\d]{3}[-\s.]?[\d]{3,6}$/', $check)))) {
+            echo '<div class="card-body alert alert-danger">';
+            $correcte = false;
+        }
+        else { echo '<div class="card-body alert alert-success">'; }
+
+        if ($check === '') { echo 'Falta el telèfon'; }
+        else { echo $check; }
+        echo '</div>'
+        ?>
     </div>
 
     <div class="card">
-        <div class="card-body" >
-            <?php echo $_GET["telefon"] ?? '';?>
-        </div>
+        <label for="form_sexe" class="form-label input-group">Sexe</label>
+        <?php
+        $check = $_GET["sexe"] ?? '';
+
+        if ($check === '') { echo '<div class="card-body alert alert-warning">Falta el sexe</div>'; }
+        else { echo '<div class="card-body alert alert-info">'.$check.'</div>'; }
+        ?>
     </div>
 
     <div class="card">
-        <div class="card-body" >
-            <?php echo $_GET["sexe"] ?? '';?>
-        </div>
+        <label class="form-label input-group" for="form_civil">Estat Civil</label>
+        <?php
+        $check = $_GET["civil"] ?? '';
+
+        if ($check === '') { echo '<div class="card-body alert alert-warning">Falta l\'estat civil</div>'; }
+        else { echo '<div class="card-body alert alert-info">'.$check.'</div>'; }
+        ?>
     </div>
+    <br>
+    <button type="submit" class="btn btn-primary">EDITAR</button>
 
-    <div class="card">
-        <div class="card-body" >
-            <?php echo $_GET["civil"] ?? '';?>
-        </div>
-    </div>
-
-    <button type="submit">EDITAR</button>
-
-    <?php if ($correcte) {
-        echo '<a href="index.html"><button type="button">OK</button></a>';
-    } ?>
+    <?php
+    if ($correcte) {
+        echo '<a href="index.html"><button class="btn btn-primary" type="button">OK</button></a>';
+    } else {
+        echo '<br><br><h2 class="alert alert-danger">Formulari incorrecte</h2>';
+    }
+    ?>
 
 </form>
 
